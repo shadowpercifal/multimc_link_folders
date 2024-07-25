0a221ac43c15ba57d894e90e982aadbe94f41108
TOR1MA <yaroslav.kurganov@gmail.com>
2024-07-26 01:57:24 +0300

    code optimization

--- src/multimc_link.py
+++ src/multimc_link.py
@@ -2,13 +2,10 @@ import os
 import sys
 
 minecraft_folder = os.environ['USERPROFILE'] + '/AppData/Roaming/.minecraft'
-folder_raw = input('Enter a file path:\n')
-folder = ''
-for char in folder_raw:
-    folder += char.replace('\\', '/')
+folder = input('Enter a file path:\n')
 subfolders = [f'{folder}/saves', f'{folder}/shaderpacks', f'{folder}/screenshots', f'{folder}/resourcepacks']
 minecraft_subfolders = [f'{minecraft_folder}/saves', f'{minecraft_folder}/shaderpacks', f'{minecraft_folder}/screenshots', f'{minecraft_folder}/resourcepacks']
-for subfolder, minecraft_subfolder in zip(subfolders, minecraft_subfolders,):
+for subfolder, minecraft_subfolder in zip(subfolders, minecraft_subfolders):
     if os.path.isdir(subfolder) is True:
         try:
             os.rmdir(subfolder)
